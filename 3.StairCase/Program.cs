d1ee0c879f66966bc86c4f308a60bea83be8c1ed
bkarnzyn <rnzgtswgg@gmail.com>
2022-03-29 23:42:58 +0800

    staircase

---
+++ 3.StairCase/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.StairCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input height: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            print(size);
+            Console.ReadKey();
+        }
+        static void print(int size)
+        {
+            for(int x = size-1; x>= 0; x--)
+            {
+                for(int j = 0; j<=size; j++)
+                {
+                    if (j > x)
+                        Console.Write("#");
+                    else
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+    }
+}
